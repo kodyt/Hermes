a21ac6130c8849f0a78e32e4af4f82fdbb7adc04
Kody Takada <kodytakada@icloud.com>
2024-03-12 17:30:27 -0400

    Initial commit

---
+++ src/pages/data-entry.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Page, Navbar, Block } from 'framework7-react';
+
+
+const EntryPage = () => {
+    return (
+      <Page name="Data Visualization">
+        <Navbar title="Data Visualization" />
+        <Block>
+            <p> Insert data entry components here </p>
+        </Block>
+      </Page>
+    );
+};
+
+export default EntryPage;
---
+++ src/pages/export.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Page, Navbar, Block } from 'framework7-react';
+
+
+const ExportPage = () => {
+    return (
+      <Page name="Data Export">
+        <Navbar title="Data Export" />
+        <Block>
+            <p> Insert data export components here </p>
+        </Block>
+      </Page>
+    );
+};
+
+export default ExportPage;
