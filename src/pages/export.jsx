e99345af44800a6be1bdc030cb871dd2162458d5
Grace <gracegyq@umich.edu>
2024-03-26 17:11:09 -0400

    export version 1

--- src/pages/export.jsx
+++ src/pages/export.jsx
@@ -6,9 +6,102 @@ const ExportPage = () => {
     return (
       <Page name="Data Export">
         <Navbar title="Data Export" />
-        <Block>
-            <p> Insert data export components here </p>
-        </Block>
+        <div class="page">
+            <div class="page-content infinite-scroll-content infinite-scroll-top">
+            <Block>
+              <Block>
+                <label>
+                  <input type="checkbox" />
+                  Medications
+                </label>
+
+                <label class="item-content">
+                  <div class="item-inner">
+                    <div class="item-title">Include all recorded times:</div>
+                    <div class="item-after">
+                      <div class="toggle toggle-init">
+                        <input type="checkbox" />
+                        <span class="toggle-icon"></span>
+                      </div>
+                    </div>
+                  </div>
+                </label>
+
+                <p>Time: </p>
+                {/* TODO: replace start & end dates */}
+                <p>Start: Monday; December 11, 2023 </p>
+                <p>End: Thursday; December 14, 2023 </p>
+              
+                <p>Tags:</p>
+
+                <p>Medicines: </p>
+                  <label>
+                    <input type="checkbox" />
+                    Lisinopril
+                  </label>
+                  <label>
+                    <input type="checkbox" />
+                    Metformin
+                  </label>
+              </Block>
+
+              <Block>
+                <label>
+                  <input type="checkbox" />
+                  Timeline
+                </label>
+                <label class="item-content">
+                  <div class="item-inner">
+                    <div class="item-title">Include all recorded times:</div>
+                    <div class="item-after">
+                      <div class="toggle toggle-init">
+                        <input type="checkbox" />
+                        <span class="toggle-icon"></span>
+                      </div>
+                    </div>
+                  </div>
+                </label>
+                <p>Time: </p>
+                {/* TODO: replace start & end dates */}
+                <p>Start: Monday; December 11, 2023 </p>
+                <p>End: Thursday; December 14, 2023 </p>
+              
+
+                <p>Tags:</p>
+                
+
+              </Block>
+
+              <Block>
+                <label>
+                  <input type="checkbox" />
+                  Graphs
+                </label>
+                <label class="item-content">
+                  <div class="item-inner">
+                    <div class="item-title">Include all recorded times:</div>
+                    <div class="item-after">
+                      <div class="toggle toggle-init">
+                        <input type="checkbox" />
+                        <span class="toggle-icon"></span>
+                      </div>
+                    </div>
+                  </div>
+                </label>
+                <p>Time: </p>
+                {/* TODO: replace start & end dates */}
+                <p>Start: Monday; December 11, 2023 </p>
+                <p>End: Thursday; December 14, 2023 </p>
+              
+                <p>Tags:</p>
+
+              </Block>
+            </Block>
+                <div class="preloader infinite-scroll-preloader"></div>
+            </div>
+        </div>
+
+
       </Page>
     );
 };
